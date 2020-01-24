b1e6c11418148c77985648756ab89f6df149576a
katebekk <85067@mail.ru>
2020-01-24 17:08:00 +1000

    first commit

---
+++ src/script/script.js
@@ -0,0 +1,10 @@
+let div = document.createElement('div');
+div.className = "main";
+let canvas = document.createElement('canvas');
+canvas.className = "canvas";
+let canvas = document.createElement('canvas');
+canvas.className = "canvas";
+function displayMain() {
+    document.body.append(div);
+    div.append(canvas);
+}
