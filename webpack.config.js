e4db4a229359f6a09e536b17ad5b03db2c6dba07
katebekk <85067@mail.ru>
2020-01-26 15:00:14 +1000

    i made it easier

--- webpack.config.js
+++ webpack.config.js
@@ -1,121 +1,89 @@
-const path = require("path");
-const fs = require("fs");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs')
 
 function generateHtmlPlugins(templateDir) {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
-    const parts = item.split(".");
-    const name = parts[0];
-    const extension = parts[1];
-    return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
-      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false
-    });
-  });
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map(item => {
+        const parts = item.split('.');
+        const name = parts[0];
+        const extension = parts[1];
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+            inject: false,
+        })
+    })
 }
+const htmlPlugins = generateHtmlPlugins('./src/html/views')
+module.exports = {
 
-const htmlPlugins = generateHtmlPlugins("./src/html/views");
+    entry:["./src/js/index.js","./src/css/style.css"],
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
 
-const config = {
-  entry: ["./src/script/index.js", "./src/scss/style.scss"],
-  output: {
-    filename: "./js/bundle.js"
-  },
-  devtool: "source-map",
-  mode: "production",
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        sourceMap: true,
-        extractComments: true
-      })
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|scss)$/,
-        include: path.resolve(__dirname, "src/scss"),
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {}
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              url: false
-            }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              sourceMap: true,
-              plugins: () => [
-                require("cssnano")({
-                  preset: [
-                    "default",
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, "src/css"),
+                use: [
                     {
-                      discardComments: {
-                        removeAll: true
-                      }
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {}
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            url: false
+                        }
                     }
-                  ]
-                })
-              ]
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true
-            }
-          }
+                ]
+            },
+            {
+                test: /\.html$/,
+                include: path.resolve(__dirname, 'src/html/includes'),
+                use: ['raw-loader']
+            },
+    ],
+   },
+    devtool: "source-map",
+    mode: "production",
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+                extractComments: true
+            })
         ]
-      },
-      {
-        test: /\.html$/,
-        include: path.resolve(__dirname, "src/html/includes"),
-        use: ["raw-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "./css/style.bundle.css"
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts",
-        to: "./fonts"
-      },
-      {
-        from: "./src/favicon",
-        to: "./favicon"
-      },
-      {
-        from: "./src/img",
-        to: "./img"
-      },
-      {
-        from: "./src/uploads",
-        to: "./uploads"
-      }
-    ])
-  ].concat(htmlPlugins)
-};
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./css/style.bundle.css"
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: "./src/img",
+                to: "./img"
+            },
+            {
+                from: "./src/js/Functions.js",
+                to: "./js"
+            },
 
-module.exports = (env, argv) => {
-  if (argv.mode === "production") {
-    config.plugins.push(new CleanWebpackPlugin());
-  }
-  return config;
+        ])
+    ].concat(htmlPlugins)
+
 };
